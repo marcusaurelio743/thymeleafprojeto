5dd6b86b83f26dc6a682c708f578d95a82cc4796
marcus aurelio <marcusaurelio743@gmail.com>
2023-09-04 20:34:19 -0300

    criando metodo de buscar pessoa por nome

--- springboot/src/main/java/curso/springboot/springboot/repository/PessoaRepository.java
+++ springboot/src/main/java/curso/springboot/springboot/repository/PessoaRepository.java
@@ -1,10 +1,17 @@
 package curso.springboot.springboot.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import curso.springboot.springboot.model.Pessoa;
 
 
 public interface PessoaRepository extends CrudRepository<Pessoa, Long>{
+	
+	@Query("select p from Pessoa p  where upper(p.nome) like upper(:nome)")
+	List<Pessoa> findPessoaByName( @Param("nome") String nome);
 
 }
--- springboot/src/main/java/curso/springboot/springboot/rest/Pessoacontroller.java
+++ springboot/src/main/java/curso/springboot/springboot/rest/Pessoacontroller.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import curso.springboot.springboot.model.Pessoa;
@@ -75,6 +76,16 @@ public class Pessoacontroller {
 		andView.addObject("pessoas",pessoaRepository.findAll());
 		andView.addObject("pessoaobj",new Pessoa());
 		return andView;
+	}
+	
+	@PostMapping("pesquisarPessoa")
+	public ModelAndView pesquisar(@RequestParam("nomePesquisa") String nomePesquisa) {
+		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+		andView.addObject("pessoas",pessoaRepository.findPessoaByName("%"+nomePesquisa+"%"));
+		andView.addObject("pessoaobj",new Pessoa());
+		return andView;
+		
+		
 	}
 
 }
